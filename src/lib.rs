d8ada3f2f7d6d64461dea00d9336523d339e65cc
Harald Maida <harald.maida@innoave.com>
2019-12-22 09:59:57 +0100

    prepare AOC boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+//! # Advent of Code 2019
+//!
+//! Santa has become stranded at the edge of the Solar System while delivering
+//! presents to other planets! To accurately calculate his position in space,
+//! safely align his warp drive, and return to Earth in time to save Christmas,
+//! he needs you to bring him measurements from fifty stars.
+//!
+//! Collect stars by solving puzzles. Two puzzles will be made available on each
+//! day in the Advent calendar; the second puzzle is unlocked when you complete
+//! the first. Each puzzle grants one star. Good luck!
+//!
+//! [Advent of Code 2019](https://adventofcode.com/2019)
+
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+#[macro_use]
+extern crate log;
+
+pub mod day01;
+
+aoc_lib! { year = 2019 }
