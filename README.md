6d3a7df9f3c51e560a632aaa558d3a280183214d
haraldmaida <harald.maida@innoave.com>
2021-11-30 21:40:44 +0100

    setup AOC 2021

---
+++ README.md
@@ -0,0 +1,21 @@
+# Advent of Code 2021 in Rust
+
+Solving the [Advent of Code 2021] puzzles in Rust.
+
+## Running the puzzle solvers
+
+This project uses the great [Cargo Advent of Code Helper] for easy setup and execution of the puzzle
+solutions.
+
+## License
+
+Licensed under the MIT license<br/>
+see [LICENSE] or https://opensource.org/licenses/MIT for details.
+
+---
+[LICENSE]: LICENSE
+
+[Advent of Code 2021]: http://adventofcode.com/2021
+[Cargo Advent of Code Helper]: https://github.com/gobanos/cargo-aoc
+
+Copyright &copy; 2021, Harald Maida
---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+//! # Advent of Code 2021
+//!
+//!
+//! [Advent of Code 2021](https://adventofcode.com/2021)
+
+#![deny(unsafe_code, unstable_features)]
+#![warn(
+    bare_trait_objects,
+    broken_intra_doc_links,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    private_doc_tests,
+    rust_2018_idioms,
+    rust_2021_incompatible_closure_captures,
+    rust_2021_incompatible_or_patterns,
+    rust_2021_prefixes_incompatible_syntax,
+    rust_2021_prelude_collisions,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    variant_size_differences
+)]
+//#![warn(unused_crate_dependencies)] //TODO uncomment eventually
+//#![warn(missing_docs)] //TODO uncomment eventually
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+aoc_lib! { year = 2021 }
