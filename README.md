beffdb02ae0e5dc22b7ef7b85e2d0e07f50c0f59
haraldmaida <harald.maida@innoave.com>
2021-12-03 21:15:58 +0100

    setup project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Advent of Code in Rust
+
+Solving the [Advent of Code] puzzles in Rust.
+
+## Running the puzzle solvers
+
+This project uses the great [Cargo Advent of Code Helper] for easy setup and execution of the puzzle
+solutions.
+
+## License
+
+Licensed under the MIT license<br/>
+see [LICENSE] or https://opensource.org/licenses/MIT for details.
+
+---
+[LICENSE]: LICENSE
+
+[Advent of Code]: http://adventofcode.com
+[Cargo Advent of Code Helper]: https://github.com/gobanos/cargo-aoc
+
+Copyright &copy; 2015-2021, Harald Maida
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+//! # Advent of Code
+//!
+//! Solutions for the [Advent of Code] puzzles.
+//!
+//! [Advent of Code](https://adventofcode.com)
+
+#![deny(unsafe_code, unstable_features)]
+#![warn(
+    bare_trait_objects,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    rust_2021_incompatible_closure_captures,
+    rust_2021_incompatible_or_patterns,
+    rust_2021_prefixes_incompatible_syntax,
+    rust_2021_prelude_collisions,
+    rustdoc::broken_intra_doc_links,
+    rustdoc::private_doc_tests,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    variant_size_differences
+)]
+//#![warn(unused_crate_dependencies)] //TODO uncomment eventually
+//#![warn(missing_docs)] //TODO uncomment eventually
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+#[cfg(feature = "event2015")]
+aoc_lib! { year = 2015 }
+
+#[cfg(feature = "event2016")]
+aoc_lib! { year = 2016 }
+
+#[cfg(feature = "event2017")]
+aoc_lib! { year = 2017 }
+
+#[cfg(feature = "event2018")]
+aoc_lib! { year = 2018 }
+
+#[cfg(feature = "event2019")]
+aoc_lib! { year = 2019 }
+
+#[cfg(feature = "event2020")]
+aoc_lib! { year = 2020 }
+
+#[cfg(any(
+    feature = "event2021",
+    not(any(
+        feature = "event2020",
+        feature = "event2019",
+        feature = "event2018",
+        feature = "event2017",
+        feature = "event2016",
+        feature = "event2015"
+    ))
+))]
+aoc_lib! { year = 2021 }
