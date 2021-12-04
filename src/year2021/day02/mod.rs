ebfddfb8253740d996b0b29371ec26812accc037
haraldmaida <harald.maida@innoave.com>
2021-12-04 19:38:29 +0100

    solve day 2 part 2, 2021

--- src/year2021/day02/mod.rs
+++ src/year2021/day02/mod.rs
@@ -41,46 +41,58 @@
 //! the planned course. What do you get if you multiply your final horizontal
 //! position by your final depth?
 //!
+//! ## Part Two
+//!
+//! Based on your calculations, the planned course doesn't seem to make any
+//! sense. You find the submarine manual and discover that the process is
+//! actually slightly more complicated.
+//!
+//! In addition to horizontal position and depth, you'll also need to track a
+//! third value, aim, which also starts at 0. The commands also mean something
+//! entirely different than you first thought:
+//!
+//! - down X increases your aim by X units.
+//! - up X decreases your aim by X units.
+//! - forward X does two things:
+//!     - It increases your horizontal position by X units.
+//!     - It increases your depth by your aim multiplied by X.
+//!
+//! Again note that since you're on a submarine, down and up do the opposite of
+//! what you might expect: "down" means aiming in the positive direction.
+//!
+//! Now, the above example does something different:
+//!
+//! - forward 5 adds 5 to your horizontal position, a total of 5. Because your
+//!   aim is 0, your depth does not change.
+//! - down 5 adds 5 to your aim, resulting in a value of 5.
+//! - forward 8 adds 8 to your horizontal position, a total of 13. Because your
+//!   aim is 5, your depth increases by `8*5=40`.
+//! - up 3 decreases your aim by 3, resulting in a value of 2.
+//! - down 8 adds 8 to your aim, resulting in a value of 10.
+//! - forward 2 adds 2 to your horizontal position, a total of 15. Because your
+//!   aim is 10, your depth increases by 2*10=20 to a total of 60.
+//!
+//! After following these new instructions, you would have a horizontal position
+//! of 15 and a depth of 60. (Multiplying these produces 900.)
+//!
+//! Using this new interpretation of the commands, calculate the horizontal
+//! position and depth you would have after following the planned course. What
+//! do you get if you multiply your final horizontal position by your final
+//! depth?
+//!
 //! [Advent of Code 2021 - Day 2](https://adventofcode.com/2021/day/2)
 
-use std::fmt;
-use std::fmt::Display;
 use std::num::ParseIntError;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Position {
-    x: i32,
-    depth: i32,
-}
-
-impl Display for Position {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}/{}", self.x, self.depth)
-    }
-}
-
-impl Default for Position {
-    fn default() -> Self {
-        Self { x: 0, depth: 0 }
-    }
-}
-
-impl Position {
-    pub fn navigate(&self, command: Move) -> Self {
-        let (x, depth) = match command {
-            Move::Forward(distance) => (self.x + distance, self.depth),
-            Move::Down(distance) => (self.x, self.depth + distance),
-            Move::Up(distance) => (self.x, self.depth - distance),
-        };
-        Self { x, depth }
-    }
-}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Move {
     Forward(i32),
     Down(i32),
     Up(i32),
+}
+
+pub trait Navigate {
+    fn navigate(&self, command: Move) -> Self;
 }
 
 #[aoc_generator(day2)]
@@ -114,15 +126,94 @@ fn parse_move(line: &str) -> Result<Move, String> {
     }
 }
 
-fn simulate_navigation(route: &[Move]) -> Position {
-    route.iter().fold(Position::default(), |position, command| {
-        position.navigate(*command)
-    })
+fn simulate_navigation<S>(route: &[Move]) -> S
+where
+    S: Navigate + Default,
+{
+    route
+        .iter()
+        .fold(S::default(), |state, command| state.navigate(*command))
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Position {
+    x: i32,
+    depth: i32,
+}
+
+impl Default for Position {
+    fn default() -> Self {
+        Self { x: 0, depth: 0 }
+    }
+}
+
+impl Navigate for Position {
+    fn navigate(&self, command: Move) -> Self {
+        match command {
+            Move::Forward(distance) => Self {
+                x: self.x + distance,
+                depth: self.depth,
+            },
+            Move::Down(distance) => Self {
+                x: self.x,
+                depth: self.depth + distance,
+            },
+            Move::Up(distance) => Self {
+                x: self.x,
+                depth: self.depth - distance,
+            },
+        }
+    }
 }
 
 #[aoc(day2, part1)]
 pub fn product_of_final_position(route: &[Move]) -> i32 {
-    let final_position = simulate_navigation(route);
+    let final_position = simulate_navigation::<Position>(route);
+    final_position.x * final_position.depth
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct AimedPosition {
+    x: i32,
+    depth: i32,
+    aim: i32,
+}
+
+impl Default for AimedPosition {
+    fn default() -> Self {
+        Self {
+            x: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
+}
+
+impl Navigate for AimedPosition {
+    fn navigate(&self, command: Move) -> Self {
+        match command {
+            Move::Forward(distance) => Self {
+                x: self.x + distance,
+                depth: self.depth + self.aim * distance,
+                aim: self.aim,
+            },
+            Move::Down(distance) => Self {
+                x: self.x,
+                depth: self.depth,
+                aim: self.aim + distance,
+            },
+            Move::Up(distance) => Self {
+                x: self.x,
+                depth: self.depth,
+                aim: self.aim - distance,
+            },
+        }
+    }
+}
+
+#[aoc(day2, part2)]
+pub fn product_of_final_position_variant2(route: &[Move]) -> i32 {
+    let final_position = simulate_navigation::<AimedPosition>(route);
     final_position.x * final_position.depth
 }
 
--- src/year2021/day02/tests.rs
+++ src/year2021/day02/tests.rs
@@ -18,7 +18,7 @@ mod part1 {
     fn simulate_navigation_example1() {
         let route = parse(EXAMPLE1);
 
-        let position = simulate_navigation(&route);
+        let position = simulate_navigation::<Position>(&route);
 
         assert_eq!(position, Position { x: 15, depth: 10 });
     }
@@ -30,5 +30,34 @@ mod part1 {
         let product = product_of_final_position(&route);
 
         assert_eq!(product, 1480518);
+    }
+}
+
+mod part2 {
+    use super::*;
+
+    #[test]
+    fn simulate_navigation_example1() {
+        let route = parse(EXAMPLE1);
+
+        let position = simulate_navigation::<AimedPosition>(&route);
+
+        assert_eq!(
+            position,
+            AimedPosition {
+                x: 15,
+                depth: 60,
+                aim: 10
+            }
+        );
+    }
+
+    #[test]
+    fn answer() {
+        let route = parse(INPUT);
+
+        let product = product_of_final_position_variant2(&route);
+
+        assert_eq!(product, 1282809906);
     }
 }
