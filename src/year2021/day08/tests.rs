c29c5283413a5ecd2d542dce9e2343fe3810def1
haraldmaida <harald.maida@innoave.com>
2021-12-09 05:01:52 +0100

    solve day 8 part 1, 2021

---
+++ src/year2021/day08/tests.rs
@@ -0,0 +1,38 @@
+use super::*;
+
+const INPUT: &str = include_str!("../../../input/2021/day8.txt");
+
+const EXAMPLE1: &str = "
+be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+";
+
+mod part1 {
+    use super::*;
+
+    #[test]
+    fn least_amount_of_fuel_linear_example1() {
+        let pattern = parse(EXAMPLE1);
+
+        let count = count_digits_1_4_7_8(&pattern);
+
+        assert_eq!(count, 26);
+    }
+
+    #[test]
+    fn answer() {
+        let pattern = parse(INPUT);
+
+        let count = count_digits_1_4_7_8(&pattern);
+
+        assert_eq!(count, 272);
+    }
+}
