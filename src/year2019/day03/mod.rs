37858b1015234b418c26710e3e7a1a4c4b24e5db
haraldmaida <harald.maida@innoave.com>
2021-12-12 07:01:53 +0100

    solve day 3 part 2, 2019

--- src/year2019/day03/mod.rs
+++ src/year2019/day03/mod.rs
@@ -67,10 +67,62 @@
 //! What is the Manhattan distance from the central port to the closest
 //! intersection?
 //!
+//! ## Part Two
+//!
+//! It turns out that this circuit is very timing-sensitive; you actually need
+//! to minimize the signal delay.
+//!
+//! To do this, calculate the number of steps each wire takes to reach each
+//! intersection; choose the intersection where the sum of both wires' steps is
+//! lowest. If a wire visits a position on the grid multiple times, use the
+//! steps value from the first time it visits that position when calculating the
+//! total value of a specific intersection.
+//!
+//! The number of steps a wire takes is the total number of grid squares the
+//! wire has entered to get to that location, including the intersection being
+//! considered. Again consider the example from above:
+//!
+//! ```text
+//! ...........
+//! .+-----+...
+//! .|.....|...
+//! .|..+--X-+.
+//! .|..|..|.|.
+//! .|.-X--+.|.
+//! .|..|....|.
+//! .|.......|.
+//! .o-------+.
+//! ...........
+//! ```
+//!
+//! In the above example, the intersection closest to the central port is
+//! reached after `8+5+5+2 = 20` steps by the first wire and `7+6+4+3 = 20`
+//! steps by the second wire for a total of `20+20 = 40` steps.
+//!
+//! However, the top-right intersection is better: the first wire takes only
+//! `8+5+2 = 15` and the second wire takes only `7+6+2 = 15`, a total of
+//! `15+15 = 30` steps.
+//!
+//! Here are the best steps for the extra examples from above:
+//!
+//! * ```text
+//!   R75,D30,R83,U83,L12,D49,R71,U7,L72
+//!   U62,R66,U55,R34,D71,R55,D58,R83
+//!   ```
+//!   = 610 steps
+//! * ```text
+//!   R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+//!   U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+//!   ```
+//!   = 410 steps
+//!
+//! What is the fewest combined steps the wires must take to reach an
+//! intersection?
+//!
 //! [Advent of Code 2019 - Day 3](https://adventofcode.com/2019/day/3)
 
 use crate::year2019::day03::wire_grid::{Direction, Distance, Move, Point};
-use hashbrown::HashSet;
+use hashbrown::{HashMap, HashSet};
 
 mod wire_grid {
     use std::fmt::{self, Display};
@@ -178,8 +230,9 @@ pub fn parse(input: &str) -> Vec<Vec<Move>> {
         .unwrap_or_else(|err| panic!("invalid input: {:?}", err))
 }
 
-fn wire_points(wire: &[Move]) -> HashSet<Point> {
-    let mut points = HashSet::new();
+fn wire_points(wire: &[Move]) -> HashMap<Point, u32> {
+    let mut points = HashMap::new();
+    let mut steps = 0;
     let mut last_point = Point::ZERO;
     for a_move in wire {
         for _ in 0..a_move.distance.0 {
@@ -201,7 +254,8 @@ fn wire_points(wire: &[Move]) -> HashSet<Point> {
                     y: last_point.y,
                 },
             };
-            points.insert(point);
+            steps += 1;
+            points.insert(point, steps);
             last_point = point;
         }
     }
@@ -210,7 +264,10 @@ fn wire_points(wire: &[Move]) -> HashSet<Point> {
 
 fn find_intersections(wire1: &[Move], wire2: &[Move]) -> HashSet<Point> {
     wire_points(wire1)
-        .intersection(&wire_points(wire2))
+        .keys()
+        .copied()
+        .collect::<HashSet<_>>()
+        .intersection(&wire_points(wire2).keys().copied().collect::<HashSet<_>>())
         .copied()
         .collect()
 }
@@ -219,12 +276,41 @@ fn find_intersections(wire1: &[Move], wire2: &[Move]) -> HashSet<Point> {
 pub fn distance_to_closest_intersection(input: &[Vec<Move>]) -> Distance {
     let wire1 = &input[0];
     let wire2 = &input[1];
-    find_intersections(&wire1, &wire2)
+    find_intersections(wire1, wire2)
         .iter()
         .filter(|point| **point != Point::ZERO)
         .map(|point| Point::ZERO.manhattan_distance(*point))
         .min()
         .unwrap_or(Distance::ZERO)
+}
+
+#[aoc(day3, part2)]
+pub fn minimal_signal_delay(input: &[Vec<Move>]) -> u32 {
+    let wire1 = &input[0];
+    let wire2 = &input[1];
+    let wire1_point_delay_map = wire_points(&wire1);
+    let wire2_point_delay_map = wire_points(&wire2);
+    let wire1_points = wire1_point_delay_map
+        .keys()
+        .copied()
+        .collect::<HashSet<_>>();
+    let wire2_points = wire2_point_delay_map
+        .keys()
+        .copied()
+        .collect::<HashSet<_>>();
+    let intersections = wire1_points.intersection(&wire2_points);
+    intersections
+        .filter(|point| **point != Point::ZERO)
+        .map(|point| {
+            wire1_point_delay_map
+                .get(point)
+                .expect("intersection is not point on wire1")
+                + wire2_point_delay_map
+                    .get(point)
+                    .expect("intersection is not point on wire2")
+        })
+        .min()
+        .expect("no result found")
 }
 
 #[cfg(test)]
--- src/year2019/day03/tests.rs
+++ src/year2019/day03/tests.rs
@@ -2,35 +2,47 @@ use super::*;
 
 const INPUT: &str = include_str!("../../../input/2019/day3.txt");
 
+const EXAMPLE1: &str = "\
+R8,U5,L5,D3
+U7,R6,D4,L4
+";
+
+const EXAMPLE2: &str = "\
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83
+";
+
+const EXAMPLE3: &str = "\
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7
+";
+
 mod part1 {
     use super::*;
 
     #[test]
     fn example1() {
-        let wire1_path = parse_line("R8,U5,L5,D3").unwrap();
-        let wire2_path = parse_line("U7,R6,D4,L4").unwrap();
+        let wires = parse(EXAMPLE1);
 
-        let result = distance_to_closest_intersection(&[wire1_path, wire2_path]);
+        let result = distance_to_closest_intersection(&wires);
 
         assert_eq!(result, Distance(6));
     }
 
     #[test]
     fn example2() {
-        let wire1_path = parse_line("R75,D30,R83,U83,L12,D49,R71,U7,L72").unwrap();
-        let wire2_path = parse_line("U62,R66,U55,R34,D71,R55,D58,R83").unwrap();
+        let wires = parse(EXAMPLE2);
 
-        let result = distance_to_closest_intersection(&[wire1_path, wire2_path]);
+        let result = distance_to_closest_intersection(&wires);
 
         assert_eq!(result, Distance(159));
     }
 
     #[test]
     fn example3() {
-        let wire1_path = parse_line("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51").unwrap();
-        let wire2_path = parse_line("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7").unwrap();
+        let wires = parse(EXAMPLE3);
 
-        let result = distance_to_closest_intersection(&[wire1_path, wire2_path]);
+        let result = distance_to_closest_intersection(&wires);
 
         assert_eq!(result, Distance(135));
     }
@@ -42,5 +54,45 @@ mod part1 {
         let result = distance_to_closest_intersection(&wires);
 
         assert_eq!(result, Distance(1431));
+    }
+}
+
+mod part2 {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let wires = parse(EXAMPLE1);
+
+        let result = minimal_signal_delay(&wires);
+
+        assert_eq!(result, 30);
+    }
+
+    #[test]
+    fn example2() {
+        let wires = parse(EXAMPLE2);
+
+        let result = minimal_signal_delay(&wires);
+
+        assert_eq!(result, 610);
+    }
+
+    #[test]
+    fn example3() {
+        let wires = parse(EXAMPLE3);
+
+        let result = minimal_signal_delay(&wires);
+
+        assert_eq!(result, 410);
+    }
+
+    #[test]
+    fn answer() {
+        let wires = parse(INPUT);
+
+        let result = minimal_signal_delay(&wires);
+
+        assert_eq!(result, 48012);
     }
 }
